77e67232241e537e7795cb322344ea72c902c326
unknown <Lucky Ecuador@GYE-IT-01.lucky.local>
2017-08-14 18:04:49 -0500

    DI AngularJS Practice

---
+++ 2 DependencyInjection/app.js
@@ -0,0 +1,13 @@
+(function(){
+	
+	angular.module('DIApp',[])
+	
+	.controller('DIController', DIController);
+	
+	//en angular $ hace referencia a un servicio
+	function DIController ($scope, $filter){
+		$scope.name="Ingi";
+		
+	}
+	
+})();
