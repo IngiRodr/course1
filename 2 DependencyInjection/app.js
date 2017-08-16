a761358136f5d2cca087e7bca7a00e888ae72b30
unknown <Lucky Ecuador@GYE-IT-01.lucky.local>
2017-08-16 17:13:33 -0500

    DI AngularJS

--- 2 DependencyInjection/app.js
+++ 2 DependencyInjection/app.js
@@ -5,8 +5,15 @@
 	.controller('DIController', DIController);
 	
 	//en angular $ hace referencia a un servicio
+	//filter es un servicio para dar formato a la data q visualiza el user
 	function DIController ($scope, $filter){
 		$scope.name="Ingi";
+		
+		$scope.upper = function(){
+			var upCase = $filter('uppercase');
+			$scope.name = upCase($scope.name);
+		};
+		
 		
 	}
 	
